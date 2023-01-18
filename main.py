a3f7f21811dc9e34676db4740f79965cb2eef9e1
Oleg <89175955650@mail.ru>
2023-01-18 15:20:50 +0300

    Commit to GitHub.

---
+++ main.py
@@ -0,0 +1,13 @@
+# Create vk-@alise_n.txt file: snscrape --max-results 10 vkontakte-user alise_n > vk-@alise_n.txt
+# snscrape --max-results 10 --jsonl vkontakte-user alise_n > vk-json@alise_n.txt
+# snscrape --max-results 10 --jsonl vkontakte-user alise_n > vk-@alise_n.json
+# snscrape --max-results 10 --jsonl vkontakte-user fitnessed > vk-fitnessed_groupe@alise_n.json
+# snscrape --max-results 10 --jsonl telegram-channel python2day > telegram@python2day.json
+# snscrape --max-results 10 --jsonl twitter-user python2day > twitter@python2day.json
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
